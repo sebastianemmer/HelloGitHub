1914709ea3f3cb6ee1825b9845e3a90a9a720211
Sebastian Emmer <sebastian.emmer@edu.campus02.at>
2016-10-01 13:35:06 +0200

    Stundenende

--- src/OperatorsArithmetic.java
+++ src/OperatorsArithmetic.java
@@ -11,6 +11,20 @@ public class OperatorsArithmetic {
 		System.out.println(a + " * " + b + " = " + (a * b));
 		System.out.println(a + " / " + b + " = " + (a / b));
 		System.out.println(a + " % " + b + " = " + (a % b));
+		
+		double tempF = celsius2Fahrenheit(26.0);
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Fahrenheit: " + tempF);
 	}
+	
+	public static double celsius2Fahrenheit(double celsius) {
+		double fahrenheit = 9.0/5 * celsius + 32;
+		return fahrenheit;
+		
+	}
+	
+// HÜ: Umgekehrte Umrechnung durchführen (Fahrenheit > Celsius)
 
 }
