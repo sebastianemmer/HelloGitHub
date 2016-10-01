9759a98d2f85923e40e5d541d118891c5426f0be
Sebastian Emmer <sebastian.emmer@edu.campus02.at>
2016-10-01 13:17:41 +0200

    TEST

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -3,8 +3,33 @@ public class HelloGitHub {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello GitHub, I'm developer 2!");
+		/* Versuche Datentypen
+		int ganzzahl = 10;
+		boolean isSunny = true;
+		float piApprox = 3.14f;
+		double euler = 2.714387869552;
+		char letterA = 'a';
 		
+		int test = 0;
+		
+		
+		System.out.println(test);
+		
+		System.out.println(ganzzahl);
+		System.out.println(isSunny);
+		System.out.println(piApprox);
+		System.out.println(euler);
+		System.out.println(letterA);
+		
+		System.out.println("---");
+		
+		System.out.println(56);
+		System.out.println(3.14);
+		System.out.println(true);
+		
+		System.out.println("Hello GitHub, I'm developer 2!");
+		System.out.println("Hello World PR 1");
+		*/
 	}
 
 }
