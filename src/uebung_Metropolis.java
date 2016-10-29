76b490c960332d3b3d02b0408f97395eb2b70845
Sebastian Emmer <sebastian.emmer@edu.campus02.at>
2016-10-29 15:22:07 +0200

    .

--- src/uebung_Metropolis.java
+++ src/uebung_Metropolis.java
@@ -2,21 +2,34 @@
 public class uebung_Metropolis {
 
 	public static void main(String[] args) {
-		Boolean ergebnis = isMetropolis(true, 250000, 5439820);
+		boolean ergebnis = isMetropolis(true, 101_000, 100);
 		
-		System.out.println("Ergebnis: "+ergebnis);
+//		System.out.println("Ergebnis: "+ergebnis);
 		
+		if (ergebnis) {
+			System.out.println("Die Metropole ist Cool.");
+		}
+		else {
+			System.out.println("Vorstadtprovinz!");
+		}
 	}
 	
 	public static boolean isMetropolis(boolean isCapital, int inhabitants, double taxPerPersonAndMonth) {
-		if ((isCapital && inhabitants >= 100000) || (inhabitants >= 200000 && (taxPerPersonAndMonth * 12) >= 720000000))
+		
+		double taxes = inhabitants * taxPerPersonAndMonth * 12;
+		
+		if ((isCapital && inhabitants > 100_000) || (inhabitants > 200_000 && taxes > 720_000_000))
 		{
 			return true;
 		}
 		else
 		{
-		return false;	
+			return false;	
+		
+//		boolean resultA = isCapital && inhabitants > 100_000;
+//		boolean resultB = taxes > 720_000_000 && inhabitants > 200_000;
+//		
+//		return resultA || resultB;
 		}
 	}
-
 }
